7086de058b7eb5cfcb802e6c126b67343e975ea5
javiercolodro <javiercolodro@gmail.com>
2024-07-27 23:31:14 -0300

    change port and host configuration

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ const app = require("./app");
 const db = require("./db");
 const globalConstants = require("./const/globalConstants");
 
-const PORT = process.env.PORT || globalConstants.PORT;
+const port = process.env.PORT || globalConstants.PORT;
+const host = "RENDER" in process.env ? "0.0.0.0" : "localhost";
 
 db.sync({ force: false }).then(() => {
-  app.listen(globalConstants.PORT, () => {
-    console.log(`La aplicación está escuchando en el puerto: ${PORT}`);
+  app.listen(port, host, () => {
+    console.log(`La aplicación está escuchando en el puerto: ${port}`);
   });
 });
